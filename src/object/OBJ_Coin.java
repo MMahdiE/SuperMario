5796371c9ec9f49f0b624cc9ae872dd0fb00874e
MMahdiE <ori76436@gmail.com>
2023-04-12 07:10:53 +0430

    non-static coins

--- src/object/OBJ_Coin.java
+++ src/object/OBJ_Coin.java
@@ -7,7 +7,14 @@ public class OBJ_Coin extends SuperObject{
     public OBJ_Coin() {
         name = "Coin";
         try {
-            image = ImageIO.read(getClass().getResourceAsStream("/objects/coin01.png"));
+            image[0] = ImageIO.read(getClass().getResourceAsStream("/objects/coin01.png"));
+            image[1] = ImageIO.read(getClass().getResourceAsStream("/objects/coin02.png"));
+            image[2] = ImageIO.read(getClass().getResourceAsStream("/objects/coin03.png"));
+            image[3] = ImageIO.read(getClass().getResourceAsStream("/objects/coin04.png"));
+            image[4] = ImageIO.read(getClass().getResourceAsStream("/objects/coin05.png"));
+            image[5] = ImageIO.read(getClass().getResourceAsStream("/objects/coin06.png"));
+            image[6] = ImageIO.read(getClass().getResourceAsStream("/objects/coin07.png"));
+            image[7] = ImageIO.read(getClass().getResourceAsStream("/objects/coin08.png"));
         }catch (IOException e) {
             e.printStackTrace();
         }
--- src/object/SuperObject.java
+++ src/object/SuperObject.java
@@ -6,16 +6,28 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class SuperObject {
-    BufferedImage image;
+    BufferedImage[] image = new BufferedImage[8];
     String name;
     boolean collision = false;
     public int worldX, y;
+    int spriteNum = 1;
+    int spriteCounter = 0;
 
     public void draw(Graphics2D g2, GamePanel gp) {
         int screenX = worldX - gp.player.worldX + gp.player.screenX;
 
         if(worldX + gp.tileWidth > gp.player.worldX - gp.player.screenX && worldX - gp.tileWidth < gp.player.worldX + gp.player.screenX) {
-            g2.drawImage(image, screenX, y, gp.tileWidth, gp.tileHeight, null);
+            spriteCounter++;
+            if(spriteCounter == gp.FPS/8) {
+                if (spriteNum != 8) {
+                    spriteNum++;
+                }
+                else {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+            g2.drawImage(image[spriteNum - 1], screenX, y, gp.tileWidth, gp.tileHeight, null);
         }
     }
 }
